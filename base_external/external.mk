f62c37674ec4e1afc2b95112261769fe7e9768d7
Alireza Mohammadkhani <Alireza.mohammadkhani@nobleo.nl>
2024-03-29 17:25:58 +0100

    update assignment 3

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,2 +1,4 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
+ARCH=arm64
+CROSS_COMPILE=aarch64-none-linux-gnu-
 #include $(BR2_EXTERNAL_project_base_PATH)/package/aesd-assignments/aesd-assignments.mk
--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '2926c56ee0b1abba746095ff5908e0b6e7312f92'
+AESD_ASSIGNMENTS_VERSION = '70148e84d6b891f5198755296bb82fb16ca0b18a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -20,6 +20,7 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa"
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
